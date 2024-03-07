a46d154dd3ab5e70a583c49000792946b602303c
Daceit <matthew.sullivan23@student.cart.org>
2024-03-07 13:34:13 -0800

    noah

--- script.js
+++ script.js
@@ -1,36 +1,66 @@
+let list = new Map();
+
+list.set(`taxes`,32.65)
+
+document.addEventListener(`DOMContentLoaded`, () => {
+
+    let elementById = function(id) {return document.getElementById(id)}
+
+    let addListener = function(id,event,code) {elementById(id).addEventListener(event, (eventData) => {code(eventData)})}
 
 
-let income = 0;
 
-document.addEventListener("DOMContentLoaded", function () {
-
-    document.getElementById('income').addEventListener('change', function (eventData) {
-
-        income = document.getElementById('income').value*(1 - document.getElementById('taxes').value/100);
-
-        console.log(income);
-        console.log(document.getElementById('housing').value !== "")
-
+    addListener(`income`,`change`, (eventData) => {
+         list.set(eventData.target.id,eventData.target.value)
     })
 
-    document.getElementById('housing').addEventListener('change', function (eventData) {
+    addListener(`taxes`,`change`, (eventData) => {
+        list.set(eventData.target.id,eventData.target.value)
+   })
 
-        console.log(document.getElementById('housing').value)
+    addListener(`housing`,`change`, (eventData) => {
+        list.set(eventData.target.id,eventData.target.value)
+   })
 
-        if(document.getElementById('housing').value !==""){// already have a value
-            console.log(income);
+   addListener(`Food`,`change`, (eventData) => {
+    list.set(eventData.target.id,eventData.target.value)
+})
 
-            income = income - document.getElementById('housing').value;
-            console.log(Math.round(income*100)/100); //two place aproximamtion
+
+
+})
+
+// let income = 0;
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     document.getElementById('income').addEventListener('change', function (eventData) {
+
+//         income = document.getElementById('income').value*(1 - document.getElementById('taxes').value/100);
+
+//         console.log(income);
+//         console.log(document.getElementById('housing').value !== "")
+
+//     })
+
+//     document.getElementById('housing').addEventListener('change', function (eventData) {
+
+//         console.log(document.getElementById('housing').value)
+
+//         if(document.getElementById('housing').value !==""){// already have a value
+//             console.log(income);
+
+//             income = income - document.getElementById('housing').value;
+//             console.log(Math.round(income*100)/100); //two place aproximamtion
     
-        }
+//         }
 
 
-    })
-
-
-
+//     })
 
 
 
-});
+
+
+
+// });
